219dd968544fb4cb756e01a37ecceb90372e1d25
Kevin McGrevey <niv3kmcg@gmail.com>
2020-04-20 18:15:49 -0600

    Adds discount index return link to discount show page with testing

--- spec/features/merchant_employee/discounts/show_spec.rb
+++ spec/features/merchant_employee/discounts/show_spec.rb
@@ -34,5 +34,28 @@ RSpec.describe "As an merchant employee", type: :feature do
       expect(page).to have_content("Minimum Item Count to Qualify: 10")
       expect(page).to have_content("Discount Percentage: 20%")
     end
+
+    it "I can return here by clicking link on a discount's show page" do
+      bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 80203)
+      employee = bike_shop.users.create!(name: "Josh Tukman",
+                                  address: "756 Main St",
+                                  city: "Denver",
+                                  state: "Colorado",
+                                  zip: "80210",
+                                  email: "josh.t@gmail.com",
+                                  password: "secret_password",
+                                  password_confirmation: "secret_password",
+                                  role: 1)
+      discount1 = bike_shop.discounts.create!(name: "Buy 5 items get 10% OFF", threshold: 5, percent_off: 10)
+      
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(employee)
+                              
+      visit merchant_discount_path(discount1)
+
+      click_link "Return to Discounts Index"
+
+      expect(current_path).to eq(merchant_discounts_path)
+    
+    end
   end
 end
